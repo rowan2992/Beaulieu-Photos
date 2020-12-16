ca63c105052307ded182310fad01b356a854a3ec
Rowan Douglas <rowan.douglas29@gmail.com>
2020-12-16 17:22:34 -0300

    can upload single photo to album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,23 @@
 class AlbumsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :show ]
+  skip_before_action :authenticate_user!, only: [ :show, :edit, :update ]
 
   def show
     @album = Album.find(params[:id])
   end
 
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update(album_params)
+      redirect_to root_path
+    end
+  end
+
   def album_params
-    params.require(:album).permit(:title, :body, :photo)
+    params.require(:album).permit(:title, :body, photos: [])
   end
 end
